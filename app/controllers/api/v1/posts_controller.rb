1f8d968554532b1b79e2df67e3938cfbe66fc8fe
Fachastorm <rodrigo.lodeiro@live.com>
2018-06-27 17:33:10 -0400

    create method in post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -7,6 +7,18 @@ class PostsController < ApplicationController
         render json: @posts 
     end
 
+    def create 
+        @post = Post.create(post_params)
+        render json: @post
+    end
+
+
+    private 
+
+    def post_params 
+        params.require(:post).permit(:title, :body)
+    end
+
 end
 
 end
